c5f440de9a71df8e53a55e2148f3d00abea12e46
scott chiang <scott01272001@gmail.com>
2023-11-29 08:09:57 +0800

    pointer

---
+++ Pointers/wallet.go
@@ -0,0 +1,25 @@
+package pointers
+
+import "fmt"
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deopsit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
