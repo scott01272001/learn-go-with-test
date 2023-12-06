6be9eea42e8f64a8627f35dd581c6115b58bdc9d
Scott Chiang <scott.chiang@ironyun.com>
2023-12-06 17:58:36 +0800

    select

---
+++ selects/racer.go
@@ -0,0 +1,33 @@
+package selects
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
+
+func Racer(a, b string) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
